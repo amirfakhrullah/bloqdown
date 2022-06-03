1ad309939c50013f66b3bbc431f6b6321459fb37
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-06-03 20:03:00 +0800

    getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
 import MetaHead from "../components/MetaHead";
+import { prisma } from "../db/client";
 
-const Home: React.FC = () => {
+const Home: React.FC<{ posts: any }> = ({ posts }) => {
   return (
     <div>
       <MetaHead title="Polley" />
       <h1 className="text-4xl font-black">Polley</h1>
+      <p>{JSON.stringify(posts)}</p>
     </div>
   );
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  const posts = await prisma.post.findMany();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
