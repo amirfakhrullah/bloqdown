774f50fcbe5851d9234049007a9d925f57b43d91
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-06-03 20:25:07 +0800

    trpc working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import MetaHead from "../components/MetaHead";
 import { prisma } from "../db/client";
+import { trpc } from "../utils/trpc";
 
 const Home: React.FC<{ posts: any }> = ({ posts }) => {
+  const { isLoading, data } = trpc.useQuery(["hello"]);
+
+  if (isLoading || !data) return  <h1 className="text-4xl font-black">Loading ...</h1>
+
   return (
     <div>
       <MetaHead title="Polley" />
       <h1 className="text-4xl font-black">Polley</h1>
+      <p>{data.greeting}</p>
       <p>{JSON.stringify(posts)}</p>
     </div>
   );
