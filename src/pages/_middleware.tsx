f8c10345c59ce00caecc1f04aab749275a772e73
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-06-04 00:15:38 +0800

    cuid cookie

--- src/pages/_middleware.tsx
+++ src/pages/_middleware.tsx
@@ -1,14 +1,15 @@
 import type { NextFetchEvent, NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import cuid from "cuid";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-    if (req.cookies["user-token"]) return;
+  if (req.cookies["user-token"]) return;
 
   // Apply cookie if doesn't have
-  const random = Math.random().toString();
+  const token = cuid();
   const res = NextResponse.redirect(req.nextUrl);
 
-  res.cookie("user-token", random, { sameSite: "strict" });
+  res.cookie("user-token", token, { sameSite: "strict" });
 
   return res;
 }
