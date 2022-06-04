8c83e7994851c2123e7dc240add6c7e2fdd4b937
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-06-04 17:29:21 +0800

    cleanup nextjs middleware

--- src/pages/_middleware.tsx
+++ src/pages/_middleware.tsx
@@ -2,16 +2,14 @@ import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 import { nanoid } from "nanoid";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  if (!req.cookies["user-token"]) {
-    const token = nanoid();
+  if (req.cookies["user-token"]) return;
+  
+  const token = nanoid();
 
-    // Redirect (to apply cookie)
-    const res = NextResponse.redirect(req.nextUrl);
+  // Redirect (to apply cookie)
+  const res = NextResponse.redirect(req.nextUrl);
 
-    res.cookie("user-token", token, { sameSite: "lax" });
+  res.cookie("user-token", token, { sameSite: "lax" });
 
-    return res;
-  } else {
-    return;
-  }
+  return res;
 }
