78f5b3549fbb22e9ea363f264d456f83635c0ab6
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-06-04 17:23:31 +0800

    update middleware

--- src/pages/_middleware.tsx
+++ src/pages/_middleware.tsx
@@ -2,9 +2,7 @@ import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 import { nanoid } from "nanoid";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  if (req.cookies["user-token"]) {
-    return;
-  } else {
+  if (!req.cookies["user-token"]) {
     const token = nanoid();
 
     // Redirect (to apply cookie)
@@ -13,5 +11,7 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
     res.cookie("user-token", token, { sameSite: "strict" });
 
     return res;
+  } else {
+    return;
   }
 }
