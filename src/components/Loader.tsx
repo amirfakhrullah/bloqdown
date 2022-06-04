2ff7d82a2aa4f2550ce7132e59010f6d1ea84879
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-06-04 23:23:19 +0800

    add Header in Loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "./Header";
 import MetaHead from "./MetaHead";
 
 const Loader: React.FC = () => {
   return (
     <div className="p-4 w-full max-w-xl mx-auto">
       <MetaHead title="Polley" />
+      <Header />
       <h1 className="text-xl font-black text-center text-gray-300">Loading ...</h1>
     </div>
   );
