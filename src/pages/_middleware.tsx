438c8a7134332954b7073d19683ddf57761fbb48
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-06-05 13:58:16 +0800

    set maxAge cookie

--- src/pages/_middleware.tsx
+++ src/pages/_middleware.tsx
@@ -3,13 +3,16 @@ import { nanoid } from "nanoid";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
   if (req.cookies["user-token"]) return;
-  
+
   const token = nanoid();
 
   // Redirect (to apply cookie)
   const res = NextResponse.redirect(req.nextUrl);
 
-  res.cookie("user-token", token, { sameSite: "lax" });
+  res.cookie("user-token", token, {
+    sameSite: "lax",
+    maxAge: 10 * 365 * 24 * 60 * 60,
+  });
 
   return res;
 }
