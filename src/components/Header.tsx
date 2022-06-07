3a8bd1988376a4862a8e19b028e38417910fa58f
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-06-07 15:18:59 +0800

    update prisma schema

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Header: React.FC<{
   displayButtons?: boolean;
 }> = ({ displayButtons = true }) => {
+  const { data: session } = useSession();
   const router = useRouter();
+
+  console.log(session)
 
   return (
     <div className="w-full bg-slate-900 border-b border-gray-800 md:p-5 p-2 flex flex-row items-center justify-between">
@@ -35,6 +39,26 @@ const Header: React.FC<{
           >
             My Posts
           </button>
+          
+          <div className="px-1" />
+
+          {session ? (
+            <button
+              type="button"
+              className="py-2 px-4 rounded-md inline-block bg-indigo-500 hover:bg-indigo-700 cursor-pointer text-sm text-white font-medium"
+              onClick={() => signOut()}
+            >
+              Sign Out
+            </button>
+          ) : (
+            <button
+              type="button"
+              className="py-2 px-4 rounded-md inline-block bg-indigo-500 hover:bg-indigo-700 cursor-pointer text-sm text-white font-medium"
+              onClick={() => signIn()}
+            >
+              Sign In
+            </button>
+          )}
         </div>
       )}
     </div>
