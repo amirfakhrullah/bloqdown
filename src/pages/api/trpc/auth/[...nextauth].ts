2e7c1c1a3a8389d76e2995878b3d2e1feb1ce52b
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-06-07 14:03:03 +0800

    next-auth init

---
+++ src/pages/api/trpc/auth/[...nextauth].ts
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import GithubProvider from "next-auth/providers/github"
+
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
+export default NextAuth({
+  // https://next-auth.js.org/configuration/providers/oauth
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+  ],
+  theme: {
+    colorScheme: "dark",
+  },
+  callbacks: {
+    async jwt({ token }) {
+      return token
+    },
+  },
+})
