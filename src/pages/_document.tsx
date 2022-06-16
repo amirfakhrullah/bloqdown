29b9faeccd379fdd7498263d9de4d43452929644
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-06-16 17:15:38 +0800

    change font display to swap

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Lobster&display=optional"
+          href="https://fonts.googleapis.com/css2?family=Lobster&display=swap"
           rel="stylesheet"
         />
       </Head>
