85bae3bb96e009533ffb1c728dd6789d9f45c379
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-06-18 01:31:41 +0800

    loaders for post/:id page

---
+++ src/components/loaders/PostDisplayLoader.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+
+const PostDisplayLoader = () => {
+  return (
+    <div className="md:col-start-2 md:col-span-2 overflow-hidden">
+      {/* post content */}
+      <div className="md:p-6 p-4 mb-3 rounded-lg bg-slate-800 border border-gray-600 overflow-hidden">
+        <div className="animate-pulse">
+          <div className="h-8 w-full bg-gray-700 rounded-sm mb-4" />
+
+          <div className="mb-4">
+            {Array.from(Array(4).keys()).map((arr) => (
+              <div
+                key={`TagsLoader1__${arr}`}
+                className="h-5 w-full bg-gray-700 rounded-sm mb-2"
+              ></div>
+            ))}
+          </div>
+
+          <div className="h-6 w-full bg-gray-700 rounded-sm my-4" />
+
+          <div className="mb-4">
+            {Array.from(Array(4).keys()).map((arr) => (
+              <div
+                key={`TagsLoader2__${arr}`}
+                className="h-5 w-full bg-gray-700 rounded-sm mb-2"
+              ></div>
+            ))}
+          </div>
+
+          <div className="h-6 w-full bg-gray-700 rounded-sm my-4" />
+
+          <div className="mb-4">
+            {Array.from(Array(4).keys()).map((arr) => (
+              <div
+                key={`TagsLoader3__${arr}`}
+                className="h-5 w-full bg-gray-700 rounded-sm mb-2"
+              ></div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/* like and authors section */}
+      <div className="animate-pulse flex flex-row justify-between my-4">
+        <div className="h-6 w-16 bg-gray-700 rounded-sm" />
+
+        <div className="flex flex-col items-end flex-1">
+          <div className="h-6 w-1/4 bg-gray-700 rounded-sm" />
+          <div className="mt-2 h-6 w-1/4 bg-gray-700 rounded-sm" />
+        </div>
+      </div>
+
+      {/* comment section */}
+      {Array.from(Array(2).keys()).map((arr) => (
+        <div
+          key={`CommentsLoader__${arr}`}
+          className="my-2 py-2 px-3 rounded-lg bg-slate-800 border border-gray-500"
+        >
+          <div className="animate-pulse">
+            <div className="flex flex-row items-center justify-between">
+              <div className="flex flex-1 flex-row items-center mb-2">
+                <div className="h-[20px] w-[20px] bg-gray-700 rounded-full" />
+                <div className="ml-2 h-3 w-1/4 bg-gray-700 rounded-sm" />
+              </div>
+
+              <div className="ml-2 h-3 w-1/4 bg-gray-700 rounded-sm" />
+            </div>
+
+            <div className="mt-4">
+              {Array.from(Array(2).keys()).map((arr) => (
+                <div
+                  key={`comm__${arr}`}
+                  className="h-5 w-full bg-gray-700 rounded-sm mb-2"
+                ></div>
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PostDisplayLoader;
--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -16,6 +16,8 @@ import Markdown from "../../components/Markdown";
 import { dateFormatter } from "../../utils/dateFormatter";
 import RightNav from "../../components/RightNav";
 import Tags from "../../components/Tags";
+import TagsLoader from "../../components/loaders/TagsLoader";
+import PostDisplayLoader from "../../components/loaders/PostDisplayLoader";
 
 const Content: React.FC<{ id: string }> = ({ id }) => {
   const {
@@ -25,11 +27,7 @@ const Content: React.FC<{ id: string }> = ({ id }) => {
   } = trpc.useQuery(["post.get-by-id", { id }]);
   const [openEdit, setOpenEdit] = useState(false);
 
-  if (isLoading) {
-    return <Loader />;
-  }
-
-  if (!post || !post.title) {
+  if (!isLoading && post === undefined) {
     return (
       <>
         <MetaHead title="Post 404 Not Found | BloqDown" />
@@ -47,90 +45,100 @@ const Content: React.FC<{ id: string }> = ({ id }) => {
 
   return (
     <>
-      <MetaHead title={`${post.title} | BloqDown`} />
+      <MetaHead title={isLoading ? "Loading.." : `${post.title} | BloqDown`} />
       <Screen>
         <Header />
         <Container className="md:grid md:grid-cols-4 md:gap-3 max-w-7xl">
-          <Tags postId={post.id!} isOwner={post.isOwner!} />
 
-          <div className="md:col-start-2 md:col-span-2 overflow-hidden">
-            <div className="md:p-6 p-4 mb-3 rounded-lg bg-slate-800 border border-gray-600 overflow-hidden">
-              <h1 className="text-3xl font-black text-white mb-4 pb-1 border-b border-gray-600">
-                {post.title}
-              </h1>
-              <Markdown>{post.description as string}</Markdown>
-            </div>
+          {isLoading ? (
+            <>
+              <TagsLoader />
+              <PostDisplayLoader />
+            </>
+          ) : (
+            <>
+              <Tags postId={post.id!} isOwner={post.isOwner!} />
 
-            <div className="flex flex-row items-center justify-between">
-              <Likes
-                postId={post.id!}
-                ownerLiked={post.ownerLiked}
-                likes={post._count?.likes!}
-                page="post"
-              />
-              <div>
-                {post.githubUser ? (
-                  <div className="flex flex-row items-center my-1 justify-end">
-                    <Image
-                      src={post.githubUser.image!}
-                      height={20}
-                      width={20}
-                      alt="github avatar"
-                      className="rounded-full"
-                    />
-                    <p className=" ml-2 text-sm font-bold text-gray-400">
-                      {post.githubUser.name}
+              <div className="md:col-start-2 md:col-span-2 overflow-hidden">
+                <div className="md:p-6 p-4 mb-3 rounded-lg bg-slate-800 border border-gray-600 overflow-hidden">
+                  <h1 className="text-3xl font-black text-white mb-4 pb-1 border-b border-gray-600">
+                    {post.title}
+                  </h1>
+                  <Markdown>{post.description as string}</Markdown>
+                </div>
+
+                <div className="flex flex-row items-center justify-between">
+                  <Likes
+                    postId={post.id!}
+                    ownerLiked={post.ownerLiked}
+                    likes={post._count?.likes!}
+                    page="post"
+                  />
+                  <div>
+                    {post.githubUser ? (
+                      <div className="flex flex-row items-center my-1 justify-end">
+                        <Image
+                          src={post.githubUser.image!}
+                          height={20}
+                          width={20}
+                          alt="github avatar"
+                          className="rounded-full"
+                        />
+                        <p className=" ml-2 text-sm font-bold text-gray-400">
+                          {post.githubUser.name}
+                        </p>
+                      </div>
+                    ) : (
+                      <p className="text-gray-500 text-sm font-bold text-right">
+                        {post.isOwner ? "By you" : "Anonymous"}
+                      </p>
+                    )}
+
+                    <p className="text-gray-500 text-sm text-right">
+                      {dateFormatter(post.created!)}
                     </p>
                   </div>
-                ) : (
-                  <p className="text-gray-500 text-sm font-bold text-right">
-                    {post.isOwner ? "By you" : "Anonymous"}
-                  </p>
+                </div>
+
+                <div className="flex justify-end items-center">
+                  {post.isOwner && (
+                    <div
+                      onClick={() => setOpenEdit(true)}
+                      className="text-sm mr-2 cursor-pointer hover:underline hover:underline-offset-1"
+                    >
+                      Edit Post
+                    </div>
+                  )}
+                  <Delete
+                    type="post"
+                    githubUser={post.githubUser}
+                    id={post.id!}
+                    isOwner={post.isOwner}
+                  >
+                    Delete Post
+                  </Delete>
+                </div>
+
+                {!isFetching && (
+                  <PostForm
+                    type="edit"
+                    open={openEdit}
+                    setOpen={setOpenEdit}
+                    inputs={{
+                      id: post.id!,
+                      title: post.title!,
+                      description: post.description!,
+                    }}
+                  />
                 )}
 
-                <p className="text-gray-500 text-sm text-right">
-                  {dateFormatter(post.created!)}
-                </p>
+                <Comments
+                  id={post.id}
+                  comments={post.comments as GetCommentsArrType}
+                />
               </div>
-            </div>
-
-            <div className="flex justify-end items-center">
-              {post.isOwner && (
-                <div
-                  onClick={() => setOpenEdit(true)}
-                  className="text-sm mr-2 cursor-pointer hover:underline hover:underline-offset-1"
-                >
-                  Edit Post
-                </div>
-              )}
-              <Delete
-                type="post"
-                githubUser={post.githubUser}
-                id={post.id!}
-                isOwner={post.isOwner}
-              >
-                Delete Post
-              </Delete>
-            </div>
-
-            {!isFetching && (
-              <PostForm
-                type="edit"
-                open={openEdit}
-                setOpen={setOpenEdit}
-                inputs={{
-                  id: post.id!,
-                  title: post.title!,
-                  description: post.description!,
-                }}
-              />
-            )}
-
-            <Comments
-              id={post.id}
-              comments={post.comments as GetCommentsArrType}
-            />
-          </div>
+            </>
+          )}
 
           <RightNav />
         </Container>
