12c69f5d37bbf62ca49dd1fe2fa4c49bc8b5fc7f
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-06-19 03:12:21 +0800

    update 'updated' data on fe

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -45,7 +45,7 @@ const PostCard: React.FC<
           {isOwner ? "By you" : "Anonymous"}
         </p>
       )}
-      <p className="text-gray-500 text-sm">{dateFormatter(created)}</p>
+      <p className="text-gray-500 text-sm">{dateFormatter(created)} {updated && "• Edited"}</p>
 
       <div>
         {tags?.map((tag, idx) => (
