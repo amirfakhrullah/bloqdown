42f16d7ad400255b3035ec9aaba64253213e3b53
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2022-12-29 21:02:45 +0800

    update nextjs version, refactor and integrate ratelimiter

---
+++ src/middleware.tsx
@@ -0,0 +1,36 @@
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { nanoid } from "nanoid";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.fixedWindow(20, "10 s"),
+});
+
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const ip = req.ip ?? "127.0.0.1";
+  if (req.nextUrl.pathname === "/api/blocked") return;
+
+  const { success, pending, limit, reset, remaining } = await ratelimit.limit(
+    `mw_${ip}`
+  );
+  ev.waitUntil(pending);
+
+  const res = success
+    ? NextResponse.next(req)
+    : NextResponse.rewrite(new URL("/api/blocked", req.url), req);
+
+  if (!req.cookies.get("user-token")) {
+    const token = nanoid();
+    res.cookies.set("user-token", token, {
+      sameSite: "lax",
+      maxAge: 10 * 365 * 24 * 60 * 60,
+    });
+  }
+
+  res.headers.set("X-RateLimit-Limit", limit.toString());
+  res.headers.set("X-RateLimit-Remaining", remaining.toString());
+  res.headers.set("X-RateLimit-Reset", reset.toString());
+  return res;
+}
